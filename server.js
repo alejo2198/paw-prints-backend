b3e08f34c31d8b5f27efee88d7d3d6d678b825ed
Alejandro Castro <alejo199821@gmail.com>
2023-03-30 14:32:44 -0400

    create filses and folers. setup knex

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+
+require('dotenv').config();
+const PORT = process.env.PORT || 8081
+
+const cors = require("cors");
+const { CORS_ORIGIN } = process.env;
+
+app.use(express.json())
+app.use(express.static('public'))
+
+app.use(cors({origin: CORS_ORIGIN}));
+//
+//remove cors origin if you want to check the api on thunderclient or postman (Reviewer note)
+
+const userRoute = require('./routes/userRoute.js');
+app.use('/user', userRoute);
+
+const doggoRoute = require('./routes/doggoRoute');
+app.use('/doggo', doggoRoute);
+
+const postRoute = require('./routes/postRoute');
+app.use('/post', postRoute);
+
+
+app.listen(PORT, () => {
+    console.log("listening on port 8080")
+})
